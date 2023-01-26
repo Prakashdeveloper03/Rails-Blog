cd9c2363def5d0638f4551024b014709cd5ef7c0
Siva Prakash <prakashdeveloper@outlook.in>
2023-01-26 08:35:30 +0530

    Added DELETE Operation & Migrated comments schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_22_024637) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_26_025123) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
